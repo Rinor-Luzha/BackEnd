b3f45d735b454bc37ef00c1f481f866d70249f62
Rinor Luzha <rl51842@ubt-uni.net>
2022-10-03 19:01:31 +0200

    BackEnd setup

---
+++ Ratings.Elefanti/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ratings.Elefanti.Models;
+
+namespace Ratings.Elefanti.Data;
+public class ApplicationDbContext : DbContext { 
+    private readonly IConfiguration _configuration;
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+    public DbSet<User> Users { get; set; }
+
+}
