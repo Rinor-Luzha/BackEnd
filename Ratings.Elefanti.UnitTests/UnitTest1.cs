f78a51ad0f48afaaa96f58a892661bf967e38cfc
Donjete Tifeku <donjete.tifeku@life.gjirafa.com>
2022-10-14 23:24:14 +0200

    Start Unit Testing

---
+++ Ratings.Elefanti.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Ratings.Elefanti.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
